565f2bc16378e37a9dbfab8b4d0a5b03516316f9
Ethan Ransdell <ethan.ransdell@live.com>
2016-10-25 16:19:54 -0400

    changed db in main activity

--- app/src/main/java/com/ethanransdell/mylists/DBHelper.java
+++ app/src/main/java/com/ethanransdell/mylists/DBHelper.java
@@ -1,6 +1,8 @@
 package com.ethanransdell.mylists;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -9,9 +11,42 @@ import android.database.sqlite.SQLiteOpenHelper;
  */
 
 public class DBHelper extends SQLiteOpenHelper {
-    public DBHelper(Context context){
-        super(context,"my_lists",null,1);
+    public DBHelper(Context context) {
+        super(context, "my_lists", null, 1);
     }
-    public void onCreate(SQLiteDatabase db) {}
-    public void onUpgrade(SQLiteDatabase database, int oldVersion, int newVersion) {}
+
+    public void onCreate(SQLiteDatabase db) {
+    }
+
+    public void onUpgrade(SQLiteDatabase database, int oldVersion, int newVersion) {
+    }
+
+    public boolean insertList(String listName) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("list_name", listName);
+        db.insert("lists", null, contentValues);
+        return true;
+    }
+
+    public boolean insertListItem(int listId, String listItemName) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("list_id", listId);
+        contentValues.put("list_item_name", listItemName);
+        db.insert("list_items", null, contentValues);
+        return true;
+    }
+
+    public Cursor getLists() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res = db.rawQuery("select * from lists", null);
+        return res;
+    }
+
+    public Cursor getListItems(int listId) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res = db.rawQuery("select * from lists where list_id = " + listId + "", null);
+        return res;
+    }
 }
--- app/src/main/java/com/ethanransdell/mylists/MainActivity.java
+++ app/src/main/java/com/ethanransdell/mylists/MainActivity.java
@@ -30,6 +30,9 @@ public class MainActivity extends AppCompatActivity {
     private Map<String, String> listsMap;
     private List<String> listsList;
 
+    Cursor dbResults;
+    private DBHelper dbh = new DBHelper(this);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,6 +50,29 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        dbh = new DBHelper(this);
+        db.execSQL("CREATE TABLE IF NOT EXISTS lists(" + BaseColumns._ID + " INTEGER PRIMARY KEY, list_name TEXT);");
+        db.execSQL("CREATE TABLE IF NOT EXISTS list_items(" + BaseColumns._ID + " INTEGER PRIMARY KEY, list_id INTEGER, list_item_name TEXT);");
+        System.out.println("Created tables.");
+        if (prefs.getBoolean("firstrun", true)) {
+            ContentValues sampleListsValues;
+            ContentValues sampleListItemsValues;
+            Long newListRowId;
+            Long newListItemRowId;
+            for (int i = 0; i < 3; i++) {
+                sampleListsValues = new ContentValues();
+                sampleListsValues.put("list_name", "Sample List " + (i + 1));
+                newListRowId = db.insert("lists", null, sampleListsValues);
+                for (int j = 0; j < 5; j++) {
+                    sampleListItemsValues = new ContentValues();
+                    sampleListItemsValues.put("list_id", newListRowId.toString());
+                    sampleListItemsValues.put("list_item_name", "Sample Item " + (j + 1));
+                    newListItemRowId = db.insert("list_items", null, sampleListItemsValues);
+                }
+            }
+            System.out.println("First run, added sample lists.");
+            prefs.edit().putBoolean("firstrun", false).commit();
+        }
     }
 
     @Override
@@ -57,36 +83,16 @@ public class MainActivity extends AppCompatActivity {
         LinearLayout previousLayout = (LinearLayout) findViewById(R.id.content_main);
         previousLayout.removeAllViews();
 
-        GetListsTask getListsTask = new GetListsTask();
-        getListsTask.execute();
-
-        try {
-            Thread.sleep(1000);
-            createListButtons();
-        } catch (Exception e) {
+        dbh = new DBHelper(this);
+        dbResults = dbh.getLists();
+        listsMap = new HashMap<>();
+        listsList = new ArrayList<>();
+        while (dbResults.moveToNext()) {
+            listsMap.put(dbResults.getString(0), dbResults.getString(1));
+            listsList.add(dbResults.getString(0));
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        Collections.sort(listsList);
+        createListButtons();
     }
 
     public void goToAddList() {
@@ -130,53 +136,5 @@ public class MainActivity extends AppCompatActivity {
             layout.addView(row);
             System.out.println("Created button for list " + listId + ": " + listsMap.get(listId));
         }
-    }
-
-    public class GetListsTask extends AsyncTask<Void, Void, Void> {
-        private SQLiteDatabase db = openOrCreateDatabase("my_lists", MODE_PRIVATE, null);
-        private Cursor listsCursor;
-
-        GetListsTask() {
-        }
-
-        protected Void doInBackground(Void... params) {
-//            db.execSQL("DROP TABLE lists;");
-//            db.execSQL("DROP TABLE list_items;");
-            db.execSQL("CREATE TABLE IF NOT EXISTS lists(" + BaseColumns._ID + " INTEGER PRIMARY KEY, list_name TEXT);");
-            db.execSQL("CREATE TABLE IF NOT EXISTS list_items(" + BaseColumns._ID + " INTEGER PRIMARY KEY, list_id INTEGER, list_item_name TEXT);");
-            System.out.println("Created tables.");
-            if (prefs.getBoolean("firstrun", true)) {
-                ContentValues sampleListsValues;
-                ContentValues sampleListItemsValues;
-                Long newListRowId;
-                Long newListItemRowId;
-                for (int i = 0; i < 3; i++) {
-                    sampleListsValues = new ContentValues();
-                    sampleListsValues.put("list_name", "Sample List " + (i + 1));
-                    newListRowId = db.insert("lists", null, sampleListsValues);
-                    for (int j = 0; j < 5; j++) {
-                        sampleListItemsValues = new ContentValues();
-                        sampleListItemsValues.put("list_id", newListRowId.toString());
-                        sampleListItemsValues.put("list_item_name", "Sample Item " + (j + 1));
-                        newListItemRowId = db.insert("list_items", null, sampleListItemsValues);
-                    }
-                }
-                System.out.println("First run, added sample lists.");
-                prefs.edit().putBoolean("firstrun", false).commit();
-            }
-            listsCursor = db.rawQuery("SELECT * FROM lists;", null);
-            System.out.println("Selected " + listsCursor.getCount() + " lists.");
-            listsMap = new HashMap<>();
-            listsList = new ArrayList<>();
-            while (listsCursor.moveToNext()) {
-                listsMap.put(listsCursor.getString(0), listsCursor.getString(1));
-                listsList.add(listsCursor.getString(0));
-            }
-            Collections.sort(listsList);
-            System.out.println("listsMap has " + listsMap.keySet().size() + " keys.");
-            db.close();
-            return null;
-        }
-
     }
 }
