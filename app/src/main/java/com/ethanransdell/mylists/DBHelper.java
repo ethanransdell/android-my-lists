39d9a835c706fc6943360b98204ebd5ab2b2d1d8
Ethan Ransdell <ethan.ransdell@live.com>
2016-10-29 21:46:38 -0400

    upgrade db method

--- app/src/main/java/com/ethanransdell/mylists/DBHelper.java
+++ app/src/main/java/com/ethanransdell/mylists/DBHelper.java
@@ -13,13 +13,46 @@ import android.provider.BaseColumns;
 
 public class DBHelper extends SQLiteOpenHelper {
     public DBHelper(Context context) {
-        super(context, "my_lists", null, 1);
+        super(context, "my_lists", null, 2);
     }
 
     public void onCreate(SQLiteDatabase db) {
+        firstRun(db);
     }
 
-    public void onUpgrade(SQLiteDatabase database, int oldVersion, int newVersion) {
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        if (oldVersion < 2) {
+            upgradeTo2(db);
+        }
+    }
+
+    public void upgradeTo2(SQLiteDatabase db) {
+        db.execSQL("ALTER TABLE lists ADD COLUMN priority INTEGER;");
+        db.execSQL("ALTER TABLE list_items ADD COLUMN priority INTEGER;");
+        db.execSQL("UPDATE lists SET priority = 1;");
+        db.execSQL("UPDATE list_items SET priority = 1;");
+    }
+
+    public boolean firstRun(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE IF NOT EXISTS lists(" + BaseColumns._ID + " INTEGER PRIMARY KEY, list_name TEXT);");
+        db.execSQL("CREATE TABLE IF NOT EXISTS list_items(" + BaseColumns._ID + " INTEGER PRIMARY KEY, list_id INTEGER, list_item_name TEXT);");
+        System.out.println("Created tables.");
+        ContentValues sampleListsValues;
+        ContentValues sampleItemsValues;
+        Long newListRowId;
+        Long newItemRowId;
+        for (int i = 0; i < 3; i++) {
+            sampleListsValues = new ContentValues();
+            sampleListsValues.put("list_name", "Sample List " + (i + 1));
+            newListRowId = db.insert("lists", null, sampleListsValues);
+            for (int j = 0; j < 5; j++) {
+                sampleItemsValues = new ContentValues();
+                sampleItemsValues.put("list_id", newListRowId.toString());
+                sampleItemsValues.put("list_item_name", "Sample Item " + (j + 1));
+                newItemRowId = db.insert("list_items", null, sampleItemsValues);
+            }
+        }
+        return true;
     }
 
     public boolean addList(String listName) {
@@ -54,36 +87,26 @@ public class DBHelper extends SQLiteOpenHelper {
 
     public Cursor getLists() {
         SQLiteDatabase db = this.getReadableDatabase();
-        Cursor results = db.rawQuery("select * from lists", null);
+        Cursor results = db.rawQuery("SELECT * FROM lists;", null);
         return results;
     }
 
     public Cursor getItems(String listId) {
         SQLiteDatabase db = this.getReadableDatabase();
-        Cursor results = db.rawQuery("select * from list_items where list_id = " + listId + "", null);
+        Cursor results = db.rawQuery("SELECT * FROM list_items WHERE list_id = " + listId + ";", null);
         return results;
     }
 
-    public boolean firstRun() {
-        SQLiteDatabase db = this.getWritableDatabase();
-//        db.rawQuery("CREATE TABLE IF NOT EXISTS lists(" + BaseColumns._ID + " INTEGER PRIMARY KEY, list_name TEXT);", null);
-//        db.rawQuery("CREATE TABLE IF NOT EXISTS list_items(" + BaseColumns._ID + " INTEGER PRIMARY KEY, list_id INTEGER, list_item_name TEXT);", null);
-        System.out.println("Created tables.");
-        ContentValues sampleListsValues;
-        ContentValues sampleItemsValues;
-        Long newListRowId;
-        Long newItemRowId;
-        for (int i = 0; i < 3; i++) {
-            sampleListsValues = new ContentValues();
-            sampleListsValues.put("list_name", "Sample List " + (i + 1));
-            newListRowId = db.insert("lists", null, sampleListsValues);
-            for (int j = 0; j < 5; j++) {
-                sampleItemsValues = new ContentValues();
-                sampleItemsValues.put("list_id", newListRowId.toString());
-                sampleItemsValues.put("list_item_name", "Sample Item " + (j + 1));
-                newItemRowId = db.insert("list_items", null, sampleItemsValues);
+    public void printTable(String tableName) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor results = db.rawQuery("SELECT * FROM " + tableName + " ORDER BY _id;", null);
+        System.out.println("CONTENTS OF " + tableName + " TABLE:");
+        while (results.moveToNext()) {
+            for (int i = 0; i < results.getColumnCount(); i++) {
+                System.out.print(results.getString(i) + "\t");
             }
+            System.out.println();
         }
-        return true;
     }
+
 }
